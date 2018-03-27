1a9bf7e23e7b44cfb9ef24ef729432acd171da17
Ryan Schlesinger <ryan@ryanschlesinger.com>
2018-03-27 10:58:44 -0700

    Remove dependency on talking to docker

--- run.sh
+++ run.sh
@@ -2,26 +2,22 @@
 export PATH=$PATH:/usr/bin:/usr/local/bin:/bin
 
 : ${BACKUP_PATH:=/backup}
-VOLUMES=$(docker inspect -f '{{range .Mounts}}{{.Source}}:{{.Destination}}{{"\n"}}{{end}}' $HOSTNAME)
+: ${SOURCE_PATH:=/source}
+VOLUMES=$(ls $SOURCE_PATH)
 
 while read -r line; do
-  regex="^/var/lib/docker/volumes/([a-zA-Z0-9_\-]+)/_data:(.+)$"
-  if [[ $line =~ $regex ]]; then
-    name=${BASH_REMATCH[1]}
-    dest=${BASH_REMATCH[2]}
-    tarball=${BACKUP_PATH}/${name}.tar.gz
-    if [ -z "$RESTORE" ]; then
-      echo "Backing up ${name} from ${dest} as ${tarball}"
-      tar -C ${dest}/.. -zcf ${tarball} $(basename ${dest})
-    else
-      echo "Restoring ${name} to ${dest} from ${tarball}"
-      if [ -f ${tarball} ]; then
-        tar -C ${dest} --strip-components 1 -zxf ${tarball}
-      else
-        echo ${tarball} does not exist, skipping.
-      fi
-    fi
+  name=$line
+  src=${SOURCE_PATH}/${name}
+  tarball=${BACKUP_PATH}/${name}.tar.gz
+  if [ -z "$RESTORE" ]; then
+    echo "Backing up ${name} from ${src} as ${tarball}"
+    tar -C ${SOURCE_PATH} -zcf ${tarball} ${name}
   else
-    continue
+    echo "Restoring ${name} to ${src} from ${tarball}"
+    if [ -f ${tarball} ]; then
+      tar -C ${src} --strip-components 1 -zxf ${tarball}
+    else
+      echo ${tarball} does not exist, skipping.
+    fi
   fi
 done <<< "$VOLUMES"

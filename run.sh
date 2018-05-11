71ae18d49f4f188c80decb2d6c7b9a81b3e17f24
Ryan Schlesinger <ryan@ryanschlesinger.com>
2018-05-11 13:51:40 -0700

    Allow disabling tar compression

--- run.sh
+++ run.sh
@@ -3,19 +3,30 @@ export PATH=$PATH:/usr/bin:/usr/local/bin:/bin
 
 : ${BACKUP_PATH:=/backup}
 : ${SOURCE_PATH:=/source}
+: ${COMPRESS:=true}
 VOLUMES=$(ls $SOURCE_PATH)
+
+if [ "$COMPRESS" != 'true' ]; then
+  backup_flags='cf'
+  restore_flags='xf'
+  tar_extension=tar
+else
+  backup_flags='zcf'
+  restore_flags='zxf'
+  tar_extension=tar.gz
+fi
 
 while read -r line; do
   name=$line
   src=${SOURCE_PATH}/${name}
-  tarball=${BACKUP_PATH}/${name}.tar.gz
+  tarball=${BACKUP_PATH}/${name}.$tar_extension
   if [ -z "$RESTORE" ]; then
     echo "Backing up ${name} from ${src} as ${tarball}"
-    tar -C ${SOURCE_PATH} -zcf ${tarball} ${name}
+    tar -C ${SOURCE_PATH} -$backup_flags ${tarball} ${name}
   else
     echo "Restoring ${name} to ${src} from ${tarball}"
     if [ -f ${tarball} ]; then
-      tar -C ${src} --strip-components 1 -zxf ${tarball}
+      tar -C ${src} --strip-components 1 -$restore_flags ${tarball}
     else
       echo ${tarball} does not exist, skipping.
     fi
